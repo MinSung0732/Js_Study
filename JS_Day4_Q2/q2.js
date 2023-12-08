d4abd578f417521f70e6a66fd5b09eed5a1fe95c
MinSung_Jo <152842047+MinSung0732@users.noreply.github.com>
2023-12-08 10:16:18 +0900

    Add files via upload

---
+++ JS_Day4_Q2/q2.js
@@ -0,0 +1,11 @@
+var random_6;
+random_6 = Math.floor(Math.random()*6)+1;
+var s;
+s="주사위를 던져 나온 숫자는 (" + random_6 + ") 입니다.<br>";
+document.write(s);
+
+var random_4;
+random_4 = Math.floor(Math.random()*4)+1;
+var s2;
+s2="주사위를 던져 나온 숫자는 (" + random_4 + ") 입니다.";
+document.write(s2);
